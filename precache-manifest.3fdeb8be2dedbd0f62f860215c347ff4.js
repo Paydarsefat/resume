a1d64a1a1ecd19b09919a527ef569b74c494fd73
mostafa <m.sharmsar@gmail.com>
2018-12-16 22:10:09 +0330

    Updates

---
+++ precache-manifest.3fdeb8be2dedbd0f62f860215c347ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "5f0e0a55c2f6aed4a668",
+    "url": "/resume/static/js/main.5f0e0a55.chunk.js"
+  },
+  {
+    "revision": "697df7bd3364dd6a6918",
+    "url": "/resume/static/js/1.697df7bd.chunk.js"
+  },
+  {
+    "revision": "5f0e0a55c2f6aed4a668",
+    "url": "/resume/static/css/main.596377f0.chunk.css"
+  },
+  {
+    "revision": "53eedd3f0b1d161d2c629ceabed75257",
+    "url": "/resume/index.html"
+  }
+];
